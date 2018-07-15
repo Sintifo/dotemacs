22b4e8f981030ef786e68dfafe7453d1f9b78e92
Sebastian Markgraf <Sebastian-Markgraf@t-online.de>
2018-07-15 15:42:31 +0200

    Change bell to blink of status line.

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -36,4 +36,14 @@
   (setq spaceline-highlight-face-func 'spaceline-highlight-face-evil-state)
   (spaceline-spacemacs-theme))
 
+
+;; Set bell to flashing of modline
+(setq ring-bell-function
+      (lambda ()
+        (let ((orig-fg (face-foreground 'mode-line)))
+          (set-face-foreground 'mode-line "#F2804F")
+          (run-with-idle-timer 0.1 nil
+                               (lambda (fg) (set-face-foreground 'mode-line fg))
+                               orig-fg))))
+
 (provide 'init-appearance)
